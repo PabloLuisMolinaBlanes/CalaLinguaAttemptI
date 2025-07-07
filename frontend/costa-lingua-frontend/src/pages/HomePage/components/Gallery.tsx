8bb63141921c874d2aac9f003a7b8e0acbda4a93
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2025-07-07 19:13:06 +0200

    Modify Gallery to be more responsive

--- frontend/costa-lingua-frontend/src/pages/HomePage/components/Gallery.tsx
+++ frontend/costa-lingua-frontend/src/pages/HomePage/components/Gallery.tsx
@@ -3,31 +3,31 @@ function Gallery() {
     return (
         <>
         <div id="gallery" className="flex flex-col pb-32 font-inter text-xl">
-            <div id="top" className="flex flex-row  pb-16">
-                <div id="left" className=" flex flex-col pl-4 w-33/100">
-                    <div id="image" className="border-1 h-100"></div>
+            <div id="top" className="flex max-lg:flex-col lg:flex-row pb-16">
+                <div id="left" className=" flex flex-col pl-4 max-lg:w-100/100 lg:w-33/100">
+                    <div id="image" className="border-1 h-100 w-100"></div>
                     <div id="text" className="pt-4">
                         <p>Get access to incredible questionnaires, curated by native speakers!</p>
                     </div>
                 </div>
                 <div id="center" className=" w-33/100"></div>
-                <div id="right" className=" flex flex-col pr-4 w-33/100">
-                    <div id="image" className="border-1 h-100"></div>
+                <div id="right" className=" flex flex-col pr-4 max-lg:w-100/100 lg:w-33/100">
+                    <div id="image" className="border-1 h-100 w-100"></div>
                     <div id="text" className="pt-4">
                         <p>Create all learning materials you need with a few clicks!</p>
                     </div>
                 </div>
             </div>
-            <div id="bottom" className="flex flex-row ">
-                <div id="left" className=" flex flex-col pl-4 w-33/100">
-                    <div id="image" className="border-1 h-100"></div>
+            <div id="bottom" className="flex max-lg:flex-col lg:flex-row ">
+                <div id="left" className=" flex flex-col pl-4 max-lg:w-100/100 lg:w-33/100">
+                    <div id="image" className="border-1 h-100 w-100"></div>
                     <div id="text" className="pt-4">
                         <p>Go check out our forums, and find a lovely community always willing to help</p>
                     </div>
                 </div>
                 <div id="center" className=" w-33/100"></div>
-                <div id="right" className=" flex flex-col pr-4 w-33/100">
-                    <div id="image" className="border-1 h-100"></div>
+                <div id="right" className=" flex flex-col pr-4 max-lg:w-100/100 lg:w-33/100">
+                    <div id="image" className="border-1 h-100 w-100"></div>
                     <div id="text" className="pt-4">
                         <p>Ask for a meeting with our native Spanish speakers for personal, one-on-one sessions; or group sessions.</p>
                     </div>
